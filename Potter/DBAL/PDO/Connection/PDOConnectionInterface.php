5b411874d885e4125fa36298fc30673b20b3c45d
Jay Potter <34577317+jaypotter@users.noreply.github.com>
2021-12-18 17:22:24 -0500

    Update PDOConnectionInterface.php

--- Potter/DBAL/PDO/Connection/PDOConnectionInterface.php
+++ Potter/DBAL/PDO/Connection/PDOConnectionInterface.php
@@ -2,9 +2,25 @@
 
 namespace Potter\DBAL\PDO\Connection;
 
+use \PDO;
+
 use Potter\DBAL\Server\DatabaseServerInterface;
 
 interface PDOConnectionInterface extends DatabaseServerInterface
 {
+    public function getDSN(): string;
 
+    public function getHandle(): PDO;
+
+    public function getPass(): string;
+
+    public function getUser(): string;
+
+    public function setDSN(string $dsn): void;
+
+    public function setHandle(PDO $pdo): void;
+
+    public function setPass(string $pass): void;
+
+    public function setUser(string $user): void;
 }

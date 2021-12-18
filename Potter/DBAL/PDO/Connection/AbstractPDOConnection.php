54665fa0d99763fce94d73f2f6234b8eeb71d39d
Jay Potter <34577317+jaypotter@users.noreply.github.com>
2021-12-18 17:22:38 -0500

    Update AbstractPDOConnection.php

--- Potter/DBAL/PDO/Connection/AbstractPDOConnection.php
+++ Potter/DBAL/PDO/Connection/AbstractPDOConnection.php
@@ -2,9 +2,22 @@
 
 namespace Potter\DBAL\PDO\Connection;
 
+use \PDO;
+
 use Potter\DBAL\Server\AbstractDatabaseServer;
 
 abstract class AbstractPDOConnection extends AbstractDatabaseServer implements PDOConnectionInterface
 {
+    use PDOConnectionTrait;
 
+    final public function connect(): void
+    {
+        $this->setHandle(
+            new PDO(
+                dsn: $this->getDsn(),
+                username: $this->getUser(),
+                password: $this->getPass()
+            )
+        );
+    }
 }

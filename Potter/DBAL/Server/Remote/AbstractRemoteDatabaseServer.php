28c3735672bac655bf9ebc451ec34d6b15abf740
Jay Potter <34577317+jaypotter@users.noreply.github.com>
2021-12-18 20:40:36 -0500

    Create AbstractRemoteDatabaseServer.php

---
+++ Potter/DBAL/Server/Remote/AbstractRemoteDatabaseServer.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace Potter\DBAL\Server\Remote;
+
+use Potter\DBAL\Server\AbstractDatabaseServer;
+
+abstract class AbstractRemoteDatabaseServer extends AbstractDatabaseServer implements RemoteDatabaseServerInterface
+{
+
+}

26b1d31e7f19ab2c13c7cfbfad60dc5cab335911
Jay Potter <34577317+jaypotter@users.noreply.github.com>
2021-12-18 20:49:55 -0500

    Create AbstractLocalDatabaseServer.php

---
+++ Potter/DBAL/Server/Local/AbstractLocalDatabaseServer.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace Potter\DBAL\Server\Local;
+
+use Potter\DBAL\Server\AbstractDatabaseServer;
+
+abstract class AbstractLocalDatabaseServer extends AbstractDatabaseServer implements LocalDatabaseServerInterface
+{
+
+}

7a84a138a25202c3fbcfea8f4af3e65ef42d0f6e
Jay Potter <34577317+jaypotter@users.noreply.github.com>
2021-12-18 20:29:44 -0500

    mysqli prefix

--- Potter/DBAL/MySQL/Connection/MySQLiConnection.php
+++ Potter/DBAL/MySQL/Connection/MySQLiConnection.php
@@ -7,4 +7,11 @@ use Potter\DBAL\Server\AbstractDatabaseServer;
 final class MySQLiConnection extends AbstractDatabaseServer implements MySQLConnectionInterface
 {
     use MySQLConnectionTrait;
+
+    private const PREFIX = 'mysqli';
+
+    public function getPrefix(): string
+    {
+        return self::PREFIX;
+    }
 }

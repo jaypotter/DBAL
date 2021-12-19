abce9e3925a73bb763824ce11d4aaea19e83f103
Jay Potter <34577317+jaypotter@users.noreply.github.com>
2021-12-18 20:58:41 -0500

    Create AbstractLocalPDOConnection.php

---
+++ Potter/DBAL/PDO/Connection/Local/AbstractLocalPDOConnection.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace Potter\DBAL\PDO\Connection\Local;
+
+use Potter\DBAL\PDO\Connection\AbstractPDOConnection;
+
+abstract class AbstractLocalPDOConnection extends AbstractPDOConnection implements LocalPDOConnectionInterface
+{
+
+}

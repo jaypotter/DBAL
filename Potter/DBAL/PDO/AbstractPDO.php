8172c0d5658feb9df1ab0637bcd366f94a6228bd
Jay Potter <34577317+jaypotter@users.noreply.github.com>
2021-12-18 20:28:53 -0500

    Update AbstractPDO.php

--- Potter/DBAL/PDO/AbstractPDO.php
+++ Potter/DBAL/PDO/AbstractPDO.php
@@ -6,5 +6,15 @@ use Potter\DBAL\Database\AbstractDatabase;
 
 abstract class AbstractPDO extends AbstractDatabase implements PDOInterface
 {
+    private const PREFIX = 'pdo';
 
+    public function getDriver(): string
+    {
+
+    }
+
+    public function getPrefix(): string
+    {
+        return self::PREFIX . '/' . $this->getDriver();
+    }
 }

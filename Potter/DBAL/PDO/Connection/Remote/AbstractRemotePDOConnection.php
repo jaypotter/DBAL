70c9def65990253f1213d853af2673dcae4c9e19
Jay Potter <34577317+jaypotter@users.noreply.github.com>
2021-12-18 20:58:38 -0500

    Create AbstractRemotePDOConnection.php

---
+++ Potter/DBAL/PDO/Connection/Remote/AbstractRemotePDOConnection.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace Potter\DBAL\PDO\Connection\Remote;
+
+use Potter\DBAL\PDO\Connection\AbstractPDOConnection;
+
+abstract class AbstractRemotePDOConnection extends AbstractPDOConnection implements RemotePDOConnectionInterface
+{
+
+}

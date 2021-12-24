eb53e1ed87b9b7e4ca37a3e3e7fdeb403e0f73f2
Jay Potter <34577317+jaypotter@users.noreply.github.com>
2021-12-24 12:19:53 -0500

    MySQLiHandleTrait

--- Potter/DBAL/MySQL/Connection/MySQLiConnection.php
+++ Potter/DBAL/MySQL/Connection/MySQLiConnection.php
@@ -8,11 +8,9 @@ use Potter\DBAL\Server\Remote\AbstractRemoteDatabaseServer;
 
 final class MySQLiConnection extends AbstractRemoteDatabaseServer implements MySQLConnectionInterface
 {
-    use MySQLConnectionTrait;
+    use MySQLConnectionTrait, MySQLiHandleTrait;
 
     private const PREFIX = 'mysqli';
-
-    private MySQLi $handle;
 
     public function __construct(string $user, string $password, string $server = 'localhost', int $port = MySQLConnectionInterface::DEFAULT_PORT)
     {
@@ -25,11 +23,13 @@ final class MySQLiConnection extends AbstractRemoteDatabaseServer implements MyS
 
     public function connect(): void
     {
-        $this->handle = new MySQLi(
-            hostname: $this->getHost(),
-            username: $this->getUser(),
-            password: $this->getPass(),
-            port: $this->getPort()
+        $this->setHandle(
+            new MySQLi(
+                hostname: $this->getHost(),
+                username: $this->getUser(),
+                password: $this->getPass(),
+                port: $this->getPort()
+            )
         );
     }
 

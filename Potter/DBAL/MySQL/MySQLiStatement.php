a843e031832130e435ceec7d79f7d685726fac90
Jay Potter <34577317+jaypotter@users.noreply.github.com>
2021-12-24 14:04:29 -0500

    Spot the bug
    Uploading for code review

--- Potter/DBAL/MySQL/MySQLiStatement.php
+++ Potter/DBAL/MySQL/MySQLiStatement.php
@@ -11,20 +11,64 @@ use Potter\DBAL\{
 
 final class MySQLiStatement extends AbstractStatement
 {
+    private const MYSQL_INT = 'i';
+    private const MYSQL_STR = 's';
+
     private mysqli_stmt $statement;
+    private array $params = [];
 
     public function __construct(MySQLiConnection $connection, string $statement)
     {
         $this->statement = $connection->getHandle()->prepare($statement);
     }
 
+    public function bindParam(int $index, int $type, mixed &$var): void
+    {
+        $this->params[$index] = [$type, &$var];
+    }
+
     public function execute(): void
     {
+        echo $this->getParamTypes();
+        if (empty($this->params)) {
+            $this->statement->execute();
+            return;
+        }
+        ksort($this->params);
+        $params = [];
+        foreach ($this->params as $param) {
+            array_push($params, $param[1]);
+        }
+        $this->statement->bind_param(
+            $this->getParamTypes(),
+            ...$params
+        );
         $this->statement->execute();
     }
 
     public function fetch(): array
     {
         return $this->statement->get_result()->fetch_all();
+    }
+
+    private function getParamTypes(): string
+    {
+        $types = '';
+        foreach ($this->params as $param) {
+            $types .= $this->matchParamType($param[0]);
+        }
+        return $types;
+    }
+
+    private function matchParamType(int $constant): string
+    {
+        switch ($constant) {
+            case self::PARAM_BOOL:
+            case self::PARAM_INT:
+                return self::MYSQL_INT;
+            case self::PARAM_STR:
+            default:
+                return self::MYSQL_STR;
+        }
     }
 }

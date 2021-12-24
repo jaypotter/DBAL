5d6db1a9ac87771c2bd1d052dc46e28df336168e
Jay Potter <34577317+jaypotter@users.noreply.github.com>
2021-12-24 14:05:20 -0500

    add PARAM_BOOL

--- Potter/DBAL/Statement/StatementInterface.php
+++ Potter/DBAL/Statement/StatementInterface.php
@@ -6,11 +6,12 @@ use Potter\DBAL\Server\DatabaseServerInterface;
 
 interface StatementInterface
 {
-    public const PARAM_STRING = 0;
-    public const PARAM_INT    = 1;
+    public const PARAM_BOOL = -1;
+    public const PARAM_STR  =  0;
+    public const PARAM_INT  =  1;
 
     public function bindParam(int $index, int $type, mixed &$var): void;
-    
+
     public function execute(): void;
 
     public function fetch(): array;

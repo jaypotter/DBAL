379c08f5b3f461507f21451dfb75d7a3913ccb59
Jay Potter <34577317+jaypotter@users.noreply.github.com>
2021-12-24 16:24:11 -0500

    Create AbstractMySQLConnection.php

---
+++ Potter/DBAL/MySQL/Connection/AbstractMySQLConnection.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace Potter\DBAL\MySQL\Connection;
+
+use Potter\DBAL\{
+    PDO\Connection\Remote\AbstractRemotePDOConnection,
+    Statement\StatementInterface
+};
+
+abstract class AbstractMySQLConnection extends AbstractRemotePDOConnection implements MySQLConnectionInterface
+{
+    private const PREFIX = 'mysql';
+
+    public function getPrefix(): string
+    {
+        return self::PREFIX;
+    }
+
+    abstract public function showDatabases(string $like = ''): array;
+}

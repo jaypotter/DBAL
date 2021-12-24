b6a3b019b1a434bfc381a4d3889617639a95cf7e
Jay Potter <34577317+jaypotter@users.noreply.github.com>
2021-12-24 12:54:50 -0500

    bindParam method

--- Potter/DBAL/Statement/StatementInterface.php
+++ Potter/DBAL/Statement/StatementInterface.php
@@ -6,7 +6,12 @@ use Potter\DBAL\Server\DatabaseServerInterface;
 
 interface StatementInterface
 {
-    public function execute(): void;
+    public const PARAM_STRING = 0;
+    public const PARAM_INT    = 1;
+
+    public function bindParam(int $index, int $type, mixed &$var): void;
     
+    public function execute(): void;
+
     public function fetch(): array;
 }

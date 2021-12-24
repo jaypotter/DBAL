306f099472400fd5248310e81341d97082ee22d9
Jay Potter <34577317+jaypotter@users.noreply.github.com>
2021-12-24 10:31:32 -0500

    Update MySQLiConnection.php

--- Potter/DBAL/MySQL/Connection/MySQLiConnection.php
+++ Potter/DBAL/MySQL/Connection/MySQLiConnection.php
@@ -2,6 +2,8 @@
 
 namespace Potter\DBAL\MySQL\Connection;
 
+use \MySQLi;
+
 use Potter\DBAL\Server\Remote\AbstractRemoteDatabaseServer;
 
 final class MySQLiConnection extends AbstractRemoteDatabaseServer implements MySQLConnectionInterface
@@ -9,6 +11,17 @@ final class MySQLiConnection extends AbstractRemoteDatabaseServer implements MyS
     use MySQLConnectionTrait;
 
     private const PREFIX = 'mysqli';
+
+    public function connect(): void
+    {
+        $this->setHandle(
+            new MySQLi(
+                hostname: $this->getHost(),
+                username: $this->getUser(),
+                password: $this->getPass()
+            )
+        );
+    }
 
     public function getPrefix(): string
     {

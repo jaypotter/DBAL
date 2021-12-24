e17d34270ab116cee97160ae481fdb3ade27280e
Jay Potter <34577317+jaypotter@users.noreply.github.com>
2021-12-24 14:06:14 -0500

    showDatabases
    WIP
    uploading for code review

--- Potter/DBAL/MySQL/Connection/MySQLConnectionTrait.php
+++ Potter/DBAL/MySQL/Connection/MySQLConnectionTrait.php
@@ -10,7 +10,15 @@ trait MySQLConnectionTrait
 
     final public function showDatabases(string $like = ''): array
     {
-        $statement = $this->prepare("SHOW DATABASES");
+        $query = "SHOW DATABASES";
+        if (strlen($like) === 0) {
+            $statement = $this->prepare($query);
+            $statement->execute();
+            return $statement->fetch();
+        }
+        $query .= " LIKE ?";
+        $statement = $this->prepare($query);
+        $statement->bindParam(1, StatementInterface::PARAM_STR, $like);
         $statement->execute();
         return $statement->fetch();
     }

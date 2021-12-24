ffa78918d62959e5a9641d427ad170d770fb153e
Jay Potter <34577317+jaypotter@users.noreply.github.com>
2021-12-24 14:32:15 -0500

    move showDatabases from trait

--- Potter/DBAL/MySQL/Connection/MySQLConnection.php
+++ Potter/DBAL/MySQL/Connection/MySQLConnection.php
@@ -2,7 +2,10 @@
 
 namespace Potter\DBAL\MySQL\Connection;
 
-use Potter\DBAL\PDO\Connection\Remote\AbstractRemotePDOConnection;
+use Potter\DBAL\{
+    PDO\Connection\Remote\AbstractRemotePDOConnection,
+    Statement\StatementInterface
+};
 
 final class MySQLConnection extends AbstractRemotePDOConnection implements MySQLConnectionInterface
 {
@@ -23,5 +26,20 @@ final class MySQLConnection extends AbstractRemotePDOConnection implements MySQL
     public function getPrefix(): string
     {
         return self::PREFIX;
+    }
+
+    final public function showDatabases(string $like = ''): array
+    {
+        $query = "SHOW DATABASES";
+        if (strlen($like) === 0) {
+            $statement = $this->prepare($query);
+            $statement->execute();
+            return $statement->fetch();
+        }
+        $query .= " LIKE ?";
+        $statement = $this->prepare($query);
+        $statement->bindParam(1, StatementInterface::PARAM_STR, $like);
+        $statement->execute();
+        return $statement->fetch();
     }
 }

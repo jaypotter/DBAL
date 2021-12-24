107d551bac9b914a24483db1304b44fd4c21312b
Jay Potter <34577317+jaypotter@users.noreply.github.com>
2021-12-24 14:05:56 -0500

    prepare method

--- Potter/DBAL/Server/DatabaseServerInterface.php
+++ Potter/DBAL/Server/DatabaseServerInterface.php
@@ -2,12 +2,15 @@
 
 namespace Potter\DBAL\Server;
 
-use Potter\{
-    Connection\ConnectionInterface,
-    DBAL\Database\DatabaseInterface
+use Potter\Connection\ConnectionInterface;
+use Potter\DBAL\{
+    Database\DatabaseInterface,
+    Statement\StatementInterface
 };
 
 interface DatabaseServerInterface extends ConnectionInterface
 {
-    public function getDatabase(string $database): DatabaseInterface;
+    //public function getDatabase(string $database): DatabaseInterface;
+
+    public function prepare(string $statement): StatementInterface;
 }

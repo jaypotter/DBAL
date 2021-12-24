6d3065f8ee1a7663a681d700d1a2bbf3aabd5dc6
Jay Potter <34577317+jaypotter@users.noreply.github.com>
2021-12-24 14:05:33 -0500

    bindParam method

--- Potter/DBAL/Statement/AbstractStatement.php
+++ Potter/DBAL/Statement/AbstractStatement.php
@@ -6,7 +6,9 @@ use Potter\DBAL\Server\DatabaseServerInterface;
 
 abstract class AbstractStatement implements StatementInterface
 {
-    abstract public function execute(): void;
+    abstract public function bindParam(int $index, int $type, mixed &$var): void;
     
+    abstract public function execute(): void;
+
     abstract public function fetch(): array;
 }
